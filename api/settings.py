f6d5e4b99bc2ccd8705c54d28c0d4e8dd81d24b3
Alex Fricker <africker@myriad.com>
2022-02-04 13:40:44 -0500

    full auth restriction

--- api/settings.py
+++ api/settings.py
@@ -133,6 +133,9 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
--- catalog/views.py
+++ catalog/views.py
@@ -7,39 +7,31 @@ from . import serializers
 class ContactTypeViewSet(viewsets.ModelViewSet):
     queryset = models.ContactType.objects.all()
     serializer_class = serializers.ContactTypeSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataFieldViewSet(viewsets.ModelViewSet):
     queryset = models.DataField.objects.all()
     serializer_class = serializers.DataFieldSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataObjectViewSet(viewsets.ModelViewSet):
     queryset = models.DataObject.objects.all()
     serializer_class = serializers.DataObjectSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataObjectTypeViewSet(viewsets.ModelViewSet):
     queryset = models.DataObjectType.objects.all()
     serializer_class = serializers.DataObjectTypeSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataSchemaViewSet(viewsets.ModelViewSet):
     queryset = models.DataSchema.objects.all()
     serializer_class = serializers.DataSchemaSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataSourceViewSet(viewsets.ModelViewSet):
     queryset = models.DataSource.objects.all()
     serializer_class = serializers.DataSourceSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class DataSourceTypeViewSet(viewsets.ModelViewSet):
     queryset = models.DataSourceType.objects.all()
     serializer_class = serializers.DataSourceTypeSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
 
 class DataSourceTypeParamsViewSet(viewsets.ModelViewSet):
     queryset = models.DataSourceTypeParams.objects.all()
     serializer_class = serializers.DataSourceTypeParamsSerializer
-    # TODO permission_classes = [permissions.IsAuthenticated]
